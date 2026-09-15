f4646c6c7ab4bd39fd58250eb81c58a1e38b85c8
munkhbayardambajav-sketch <munkhbayar.dambajav@gmail.com>
2026-09-15 03:19:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -307,7 +307,7 @@ function getPageToken(pageId) {
 }
 
 async function sendBankInfo(recipientId, pageToken) {
-      await sendFbMessage(recipientId, `https://kino-hub.mn/ энэ линкээр орж киногоо үзээрэй`);
+      await sendFbMessage(recipientId, `https://kino-hub.mn/ сайтаар орж киногоо үзээрэй. Safari болон Chrome-оор сайтруугаа хандаарай.`);
 }
 
 async function handlePaymentScreenshot(senderId, imageUrl, pageToken) {
